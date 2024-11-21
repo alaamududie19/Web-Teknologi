5548d96c79c921819716af78ea49670a79aad93c
alaamududie19 <maududie1@gmail.com>
2024-11-21 15:36:36 +0700

    first commit

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -15,16 +15,17 @@ export default function Page() {
           height={360}
           className="hidden md:block"
           alt="Banner UIA"
-        />
+          />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Selamat Datang!</strong> ini adalah situs web pembelajaran{' '}
+            <strong>Selamat Datang!</strong> <br></br>Ini adalah situs web pembelajaran{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Mata Kuliah Web Teknologi
+              Mata kuliah web teknologi
             </a>
-            , Dosen Pengampu <b>Aditya Ahmad Praditya, S.Kom., M.T.I.</b>
+            , Dosen Pengampu <b>Adhitya Ahmad Pradyta, S.Kom, M.T.I</b>.
           </p>
           <Link
             href="/login"
