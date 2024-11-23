3c337d01086c1085c38b56d98e324e501deaeb72
alaamududie19 <maududie1@gmail.com>
2024-11-23 16:49:46 +0700

    update commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
-
         <Image
         src="/logo/uia-logo.png"
         width={500}
