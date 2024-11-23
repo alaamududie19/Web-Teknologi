de83f6cf4de97212eca24b7975d1749bf1bb0df9
alaamududie19 <maududie1@gmail.com>
2024-11-23 19:55:00 +0700

    update commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,26 @@
+'use client'; //pengunaan useactionstate harus mencantumkan ini
+
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import {
     AtSymbolIcon,
     KeyIcon,
+    ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
+import { useActionState } from 'react';
+import { authenticate } from '../lib/actions'; //autentikasi
 
 export default function Page() {
+    const [errorMessage, formAction, isPending]= useActionState(
+        authenticate,
+        undefined,
+    );
     return (
+        //baris kode header
         <main className="flex min-h-screen flex-col p-6">
-            <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+            <div className="flex h-20 shrink-e items-end rounded-lg bg-blue-500 p-4 md:h-52">
                 <Image
                     src="/logo/uia-logo.png"
                     width={500}
@@ -19,17 +29,20 @@ export default function Page() {
                     alt="Banner UIA"
                 />
             </div>
-            <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20 m-auto'>
-                <form className='space-y-3'>
+
+            {/*baris kode form login*/}
+            <div className="flex flex-col justify-center gap-6 rounded-1g bg-gray-50 px-6 py-10 md:w-2/5 md:px-20 m-auto">
+                <form action={formAction} className="space-y-3">
                     <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
                         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-                            Silahkan Masukan Account Anda.
+                            Silakan Masuk dengan akun Anda.
                         </h1>
-                        <div className='w-full'>
-                            <label
+                        <div className="w-full">
+                            <div>
+                                <label
                                 className="mb-3 mt-5 block text-xs font-medium text-gray-900"
                                 htmlFor="email"
-                            >
+                    >
                                 Email
                             </label>
                             <div className="relative">
@@ -41,14 +54,14 @@ export default function Page() {
                                     placeholder="Ketikkan alamat email"
                                     required
                                 />
-                                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            <label
-                                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray"></AtSymbolIcon>
+                    </div>
+                    </div>
+                    <div className="mt-4">
+                                <label
+                                    className="mb-3 mt-5 block text-xs font-medium text-gray-900"
                                 htmlFor="password"
-                            >
+                    >
                                 Kata Sandi
                             </label>
                             <div className="relative">
@@ -59,20 +72,28 @@ export default function Page() {
                                     name="password"
                                     placeholder="Ketikkan Kata Sandi"
                                     required
-                                    minLength={6}
-                                />
-                                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                                    minLength={6}   
+                    />
+                                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus: text-gray-900"></KeyIcon>
                             </div>
                         </div>
                     </div>
-                    <Button className='mt-4 w-full'>
-                        Masuk <ArrowRightIcon className='ml-auto h-5 w-5 text-gray-50' />
+                    <Button className="mt-4 w-full" aria-disabled={isPending}>
+                    Masuk <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
                     </Button>
-                    <div className='flex h-8 items-end space-x-1'>
-                        {/* add form errors here */}
+                    <div className="flex h 8 items end space-x-1">
+
+                        {errorMessage && (
+                            <>
+                            <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                            <p className="text-sm text-red-500">{errorMessage}</p>
+                            </>
+                        )}
                     </div>
-                </form>
             </div>
-        </main>
+        </form>
+        </div>
+</main >
+
     );
 }
